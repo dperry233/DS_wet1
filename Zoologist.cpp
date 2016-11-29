df6789d0e398149f94138507742aee24cd810cd6
Krumpet <ran.lottem@campus.technion.ac.il>
2016-11-30 00:03:46 +0200

    Started working on Zoologist and derived animal class

--- Zoologist.cpp
+++ Zoologist.cpp
@@ -7,4 +7,13 @@
 #include "avlTree.h"
 
 explicit SuperBeast::SuperBeast (int id, int level = 0, SuperBeast *first = nullptr, SuperBeast *second = nullptr,
-                        Zoologist *Owner = nullptr) : Animal(id, level), beast1(first), beast2(second), Owner(Owner) {}
+                                 Zoologist *Owner = nullptr) : Animal(id, level), beast1(first), beast2(second),
+                                                               Owner(Owner) {}
+
+~SuperBeast () {};
+
+SuperBeast (const SuperBeast &original) : Animal(original), beast1(original.beast1), beast2(original.beast2),
+                                          Owner(original.owner) {}
+
+explicit SuperBeast (const Animal &animal, SuperBeast *first = nullptr, SuperBeast *second = nullptr,
+                     Zoologist *owner = nullptr) : Animal(original), beast1(first), beast2(second), Owner(owner) {}
