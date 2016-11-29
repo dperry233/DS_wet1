9fbdfea8a487460a2fef9b5a7be29dfb8b23e902
Krumpet <ran.lottem@campus.technion.ac.il>
2016-11-29 16:24:15 +0200

    Some updates on animal class - c'tors d'tor getters setters

--- Animal.cpp
+++ Animal.cpp
@@ -12,3 +12,31 @@ Animal::Animal(int level, int id) : Level(level), ID(id) {
     if (level < 0 || id <= 0) throw badAnimalInput;
 }
 
+// explicit c'tor for Animal with 0 danger level
+explicit Animal::Animal(int id) : Level(0), ID(id) {}
+
+// copy c'tor
+Animal::Animal(const Animal &original) : Level(original.Level), ID(original.ID) {}
+
+Animal::~Animal() {};
+
+int Animal::getLevel() {
+    return level;
+}
+
+int Animal::getID() {
+    return ID;
+}
+
+
+// TODO: maybe change to regular changeLevel and throw error on delta<=0 only from zoologist
+void Animal::increaseLevel(int delta) {
+    if (delta <= 0) throw badAnimalInput;
+    level += delta;
+    return;
+}
+
+void Animal::setID(int newID) {
+    if (newID <= 0) throw badAnimalInput;
+    ID = newID;
+}
--- Animal.h
+++ Animal.h
@@ -8,7 +8,17 @@ private:
 // comment to see if this works
 
 public:
-    Animal(int level, int id);
+    Animal(int level, int id); // default c'tor
+    explicit Animal::Animal(int id); // default c'tor with default Level
+    Animal::Animal(const Animal &original); // copy c'tor
+    ~Animal();
+    int getLevel();
+    int getID();
+
+    // Throws badAnimalInput if delta <= 0
+    void increaseLevel(int delta);
+
+    void setID(int newID);
 };
 
 /*
@@ -18,10 +28,10 @@ public:
 class badAnimalInput : public std::exception {
 };
 
-class Failure : public std::exception {
+/*class Failure : public std::exception {
 };
 
 class Memory : public std::exception {
 };
-
+*/
 #endif
